b02962c899d7866ef59172eea30b00df68490582
shivam agarwal <shivamagarwaloff@gmail.com>
2023-03-29 16:03:23 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GenerateOTP from './components/GenerateOTP';
 import SubmitAttendance from './components/SubmitAttendance';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/generateOTP" component={GenerateOTP} />
-        <Route path="/submitAttendance" component={SubmitAttendance} />
-      </Switch>
+      <Routes>
+        <Route path="/generateOTP" element={<GenerateOTP></GenerateOTP>} />
+        <Route path="/submitAttendance" component={<SubmitAttendance></SubmitAttendance>} />
+      </Routes>
     </Router>
   );
 }
