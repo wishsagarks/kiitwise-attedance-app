263b72f4dc5c512897330129d35c09e784312956
SAGAR SATAPATHY <wishsagarks@gmail.com>
2023-03-31 02:09:20 +0530

    submit Attendance working fine ,student login to be added
    ui needs to be added

--- src/components/SubmitAttendance.js
+++ src/components/SubmitAttendance.js
@@ -21,8 +21,8 @@ const SubmitAttendance = () => {
           longitude: lon,
         };
 
-        // Replace with your backend API endpoint
-        const response = await axios.post('/api/students/submitAttendance',studentData);
+        // Replace with  backend API endpoint
+        const response = await axios.post('http://localhost:5000/api/students/submitAttendance',studentData);
 
         setMessage(response.data.message);
       },
