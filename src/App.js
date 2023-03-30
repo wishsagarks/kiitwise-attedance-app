213f8352e892ae87b570c46cd55dfe22a32d14ab
SAGAR SATAPATHY <wishsagarks@gmail.com>
2023-03-30 16:22:55 +0530

    useHIstory needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GenerateOTP from './components/GenerateOTP';
-import SubmitAttendance from './components/SubmitAttendance';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import GenerateOTP from "./components/GenerateOTP";
+import SubmitAttendance from "./components/SubmitAttendance";
+import TeacherLogin from "./components/TeacherLogin";
+import StudentLogin from "./components/StudentLogin";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/generateOTP" element={<GenerateOTP></GenerateOTP>} />
-        <Route path="/submitAttendance" component={<SubmitAttendance></SubmitAttendance>} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/teacher-login" element={<TeacherLogin></TeacherLogin>} />
+          <Route path="/student-login" element={<StudentLogin></StudentLogin>} />
+          <Route path="/generateOTP" element={<GenerateOTP></GenerateOTP>} />
+        <Route path="/submitAttendance" element={<SubmitAttendance></SubmitAttendance>} />
+        </Routes>
+      </div>
     </Router>
   );
 }
